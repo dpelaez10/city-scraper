05b389ab946368378b003724d05ecce9f390027b
dpelaez10 <dppelaez@wpi.edu>
2021-09-20 00:51:02 -0400

    exception

--- public-works-dept.py
+++ public-works-dept.py
@@ -3,7 +3,7 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
-with open('1k-cities-pwd.csv', 'r') as f:
+with open('test.csv', 'r') as f:
     reader = csv.reader(f)
     queue = 1 
     total_emails = 0
@@ -14,18 +14,21 @@ with open('1k-cities-pwd.csv', 'r') as f:
         city = row[0]
         state = row[1]
         URL = row[2]
-        print(city + ", " + state + " (" + str(queue) + " of 13454)")
+        print('\033[1m' + city + ", " + state + '\033[0m' + " (" + str(queue) + " of 13454)")
         print("Searching " + URL)
-        page = requests.get(URL)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        mailtos = soup.select('a[href^=mailto]')
-        for i in mailtos:
-            href=i['href']
-            try:
-                str1, str2 = href.split(':')
-            except ValueError:
-                break
-            emails.append(str2)
+        try:
+            page = requests.get(URL)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            mailtos = soup.select('a[href^=mailto]')
+            for i in mailtos:
+                href=i['href']
+                try:
+                    str1, str2 = href.split(':')
+                except ValueError:
+                    break
+                emails.append(str2)
+        except:
+            pass
         email_string = ';'.join(emails)
         if len(emails) > 0:
             for email in emails:
